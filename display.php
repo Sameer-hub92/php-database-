1e9304470f3725a5ddcab15bd644dfbd03ed666b
sameer vats <sameervats13@gmail.com>
2025-01-08 08:21:52 +0530

    some inportant modification on fetching data with the help of datatable using jquery and css links

--- display.php
+++ display.php
@@ -4,9 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.2.0/css/dataTables.dataTables.css"/>
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdn.datatables.net/2.2.0/js/dataTables.js"></script>
+
 </head>
 <body>
-    <table border="1" align="center">
+
+<div class="contaoner">
+    <div class="rows">
+        <h1 class="text-center">Student record list</h1>
+        <div class="col-sm-8 offset-sm-2 mt-4">
+        <table id="ankit" border="1" align="center">
+        <thead>
         <tr>
             <th>Sno</th>
             <th>Student Name</th>
@@ -14,6 +27,9 @@
             <th>Studebt Roll</th>
             <th>Studebt Address</th>
         </tr>
+</thead>
+
+<tbody>
 <?php
 include "config.php";
 $q="select * from student";
@@ -42,6 +58,21 @@ while($rows=mysqli_fetch_array($z))
 }
 ?>
 
+</tbody>
+
 </table>
+
+
+</div>
+</div>
+</div>>
+    
+
+<script>
+   
+   new DataTable('#ankit', {
+    order: [[3, 'desc']]
+});
+    </script>
 </body>
 </html>
